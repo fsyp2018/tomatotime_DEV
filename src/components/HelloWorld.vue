b37a4f7985ffaa3d2861cfe878df88c3d6228a2d
fsyp2018 <fsyp2000@gmail.com>
2020-03-26 16:47:15 +0800

    第一次切板

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,14 +1,48 @@
 <template>
-  <div>
-123
+  <div class="bgcolor">
+    <div class="d-flex flex-row-reverse bd-highlight mr-5 pt-3">
+    <p class="text-white">{{fullDate}}</p>
+    </div>
+    <div class="header row justify-content-around">
+      <div>
+        <div class="clock border border-primary">{{minutes}}:{{seconds}}</div>
+      </div>
+      <div class="h1 text-white border border-primary">休息是為了走更遠的路</div>
+    </div>
   </div>
 </template>
 
 <script>
+import $ from 'jquery';
+
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String,
+  data() {
+    return {
+      isStart: false,
+      setSecond: 1500, // 初始共幾秒,
+      minutes: '25', // 字幕顯示(分)
+      seconds: '00', // 字幕顯示(秒)
+      date: new Date(),
+      interval: '',
+    };
+  },
+  methods: {
+    openModal() {
+      $('#exampleModal').modal('show');
+    },
+  },
+  computed: {
+    fullDate() {
+      const year = this.date.getFullYear();
+      const month = this.date.getMonth() + 1;
+      const day = this.date.getDate();
+      const hour = this.date.getHours();
+      const min = this.date.getMinutes();
+      const str = `${year}.${month}.${day} ${hour < 10 ? '0' : ''}
+      ${hour}:${min < 10 ? '0' : ''}${min}`;
+      return str;
+    },
   },
 };
 </script>
